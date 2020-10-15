9b402b03241fdcbdaffd8ff237fc9c13ec1b9124
Kenzo <69363380+kenkenkon23@users.noreply.github.com>
2020-10-15 18:16:36 -0300

    aula 4 :art:

---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports =[
+    {
+        id:1, 
+        lat: "-23.5662359",
+        lng: "-46.6421534",
+        name: "Lar das Meninas",
+        description: "Presta assistencia a meninas de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1594753154778-273013529793?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1596443686812-2f45229eebc3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "1",
+    },
+    {
+        id:2, 
+        lat: "-23.5652359",
+        lng: "-46.6441534",
+        name: "Lar dos Meninos",
+        description: "Presta assistencia a meninos de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1594753154778-273013529793?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1596443686812-2f45229eebc3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "0",
+    }
+]
---
+++ src/server.js
@@ -0,0 +1,30 @@
+//importar dependencias (libs)
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js')
+//iniciando o express (lib)
+const server = express();
+
+server
+//usar arquivos estáticos
+.use(express.static('public'))
+
+//configurar template engine
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+
+//criar uma rota
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+//ligar o servidor
+server.listen(5500);
+
+//control C no bash desliga o servidor
+//npm install express
+//npm install nodemon
+//npm install hbs
+
+//npm start
